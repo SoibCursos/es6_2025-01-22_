1e4b724704c9328184139e683c77b67fbfda87a7
Arturo <soibcursos@gmail.com>
2025-01-22 17:51:27 +0100

    antes de examen_1

--- 03-number/js/app-01.js
+++ 03-number/js/app-01.js
@@ -18,16 +18,16 @@ resultado = 20 % 10; // devuelve el resto de 20 / 10
 
 //(Libería) Objeto Math
 resultado = Math.PI;
-resultado = Math.round(2.5);
-resultado = Math.ceil(2.2);
-resultado = Math.floor(2.8);
+resultado = Math.round(2.5);//redondea al más cercano
+resultado = Math.ceil(2.2);//redondea hacia arriba
+resultado = Math.floor(2.8);//redondea hacia abajo
 
 resultado = Math.min(3, 5, 10, -1, 7);
 resultado = Math.max(3, 5, 10, -1, 7);
 
 resultado = Math.random();
 //Generar un numero aleatorio dentro de un rango de 1 a 6
-resultado = Math.floor(Math.random() * 6) + 1;
+resultado = Math.floor(Math.random() * 6) + 1;//Genera un número entre 0 y 6 y lo redondea hacia abajo
 
 //Precedéncia de operadores
 resultado = 20 + 30 * 6; //200
@@ -63,15 +63,18 @@ const n2 = "34€";
 const n3 = "uno";
 const n4 = 12;
 
-console.log(typeof +n1);
-console.log(parseInt(n1));
-console.log(parseInt(n2));
-console.log(parseFloat(n1));
-console.log(Number.parseFloat(n3));
+console.log(typeof +n1);//Nos informa del tipo de dato
+console.log(parseInt(n1));//Convierte a entero
+console.log(parseInt(n2));//Convierte a entero
+console.log(parseFloat(n1));//Convierte a decimal
+console.log(Number.parseFloat(n3));//Convierte a decimal
 
-console.log(Number.isInteger(n1));
+console.log("Nos informa si es un número entero=> " + Number.isInteger(n1));//Nos informa si es un número entero
 
-//Revisar si el cambio a numero ha ido bién
+console.log("Revisar si el cambio a numero ha ido bién");
+console.log(n3);
+console.log(parseInt(n3));
+console.log(Number.parseInt(n3));
 console.log(Number.isNaN(Number.parseInt(n3)));
 
 // window.alert("hola");
